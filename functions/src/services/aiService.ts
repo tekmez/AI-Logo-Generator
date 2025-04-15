4419e61f0a5620d3b089cdfb445376867be273ac
Tuncay Ekmez <tuncayekmez@gmail.com>
2025-04-15 15:06:23 +0300

    refactor: Generate service

--- functions/src/services/aiService.ts
+++ functions/src/services/aiService.ts
@@ -1,5 +1,4 @@
 import { createFalClient } from "@fal-ai/client";
-import { onCall } from "firebase-functions/https";
 
 interface FalResult {
   data: {
@@ -9,57 +8,52 @@ interface FalResult {
   };
 }
 
-interface GenerateLogoRequest {
-  prompt: string;
-}
+// Generate Logo with Fal AI
+export async function generateLogo(
+  prompt: string,
+  apiKey: string
+): Promise<string> {
+  if (!apiKey) {
+    throw new Error("AI_LOGO_API_KEY is not set");
+  }
 
-// Mevcut Logo Generation API
-export const generateLogo = onCall(
-  {
-    cors: true,
-    maxInstances: 10,
-    secrets: ["AI_LOGO_API_KEY"],
-  },
-  async (request) => {
-    const falApiKey = process.env.AI_LOGO_API_KEY;
-    if (!falApiKey) {
-      throw new Error("AI_LOGO_API_KEY is not set");
+  const fal = createFalClient({
+    credentials: apiKey,
+  });
+
+  const { request_id: requestId } = await fal.queue.submit(
+    "fal-ai/flux/schnell",
+    {
+      input: {
+        prompt: prompt,
+      },
     }
-    const fal = createFalClient({
-      credentials: falApiKey,
-    });
-    const { prompt } = request.data as GenerateLogoRequest;
-    const { request_id: requestId } = await fal.queue.submit(
-      "fal-ai/flux/schnell",
-      {
-        input: {
-          prompt: prompt,
-        },
-      }
-    );
+  );
 
+  const { status } = await fal.queue.status("fal-ai/flux/schnell", {
+    requestId: requestId,
+    logs: true,
+  });
+
+  while (status !== "COMPLETED") {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
     const { status } = await fal.queue.status("fal-ai/flux/schnell", {
       requestId: requestId,
       logs: true,
     });
-
-    while (status !== "COMPLETED") {
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-      const { status } = await fal.queue.status("fal-ai/flux/schnell", {
-        requestId: requestId,
-        logs: true,
-      });
-      if (status === "COMPLETED") {
-        break;
-      }
+    if (status === "COMPLETED") {
+      break;
     }
-    const result = await fal.queue.result("fal-ai/flux/schnell", {
-      requestId: requestId,
-    });
-    const outputUrl = (result as FalResult)?.data?.images?.[0]?.url;
-    if (!outputUrl) {
-      throw new Error("Process failed");
-    }
-    return { success: true, logoUrl: outputUrl };
   }
-);
+
+  const result = await fal.queue.result("fal-ai/flux/schnell", {
+    requestId: requestId,
+  });
+
+  const outputUrl = (result as FalResult)?.data?.images?.[0]?.url;
+  if (!outputUrl) {
+    throw new Error("Process failed");
+  }
+
+  return outputUrl;
+}
